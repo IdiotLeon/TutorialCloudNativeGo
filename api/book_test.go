7e9e2e66708fc2ed9f30c0770248305f8ace4e3f
Leon <yanglyu.leon.7@gmail.com>
2019-12-04 21:25:05 -0800

    tutorial: Marshalling and unmarshalling JSON of Go structs
    module: 2.1

---
+++ api/book_test.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBookToJSON(t *testing.T) {
+	book := Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"}
+	json := book.ToJSON()
+
+	assert.Equal(t,
+		`{"title":"Cloud Native Go","author":"M.-L. Reimer","isbn":"0123456789"}`,
+		string(json),
+		"Book JSON marshalling wrong.")
+}
+
+func TestBookFronJSON(t *testing.T) {
+	json := []byte(`{"title":"Cloud Native Go","author":"M.-L. Reimer","isbn":"0123456789"}`)
+	book := FromJSON(json)
+
+	assert.Equal(t,
+		Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"},
+		book,
+		"Book JSON unmarshalling wrong.")
+}
